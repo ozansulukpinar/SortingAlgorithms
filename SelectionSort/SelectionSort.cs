49e59fe178720f14011ddae2a19a501059cd068a
ozansulukpinar <ozansulukpinar@hotmail.com>
2021-06-02 01:10:48 +0300

    Commit #1 - Folders and class added

---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,24 @@
+public static void SelectionSort(int[] input)
+{
+    // TODO: Debugging
+    int length = input.Length;
+    int index = 0;
+    bool isItChanged = false;
+
+    for(int i = 0; i < length; i++){
+        int smallestItem = input[i];
+        
+        for(int j = (i + 1); j < length; j++){
+            if(input[j] < smallestItem){
+                smallestItem = input[j];
+                index = j;
+                isItChanged = true;
+            }
+
+            if(j == (length - 1) && isItChanged){
+                input[index] = input[i];
+                input[i] = smallestItem;
+            }
+        }
+    }
+}
